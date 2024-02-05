60877e729250b4ed959c7b44597f71bcba640c21
Anna Lavrova <anna.laurier99@gmail.com>
2024-02-05 15:18:03 +0300

    Randomiser - HM 1

--- Randomiser/ViewController.swift
+++ Randomiser/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximumValueLabel: UILabel!
+    @IBOutlet weak var randomValueLabel: UILabel!
+
+    @IBOutlet weak var getNumberButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getNumberButton.layer.cornerRadius = 10
     }
 
+
+    @IBAction func getRandomNumberTapped() {
+        let minimumNumber = Int(minimumValueLabel.text ?? "") ?? 0
+        let maximumNumber = Int(maximumValueLabel.text ?? "") ?? 100
+
+        randomValueLabel.text = String(Int.random(in: minimumNumber ... maximumNumber))
+    }
+    
 
 }
 
